680613d3a2c91a2e4dbf9b7531d60151e43d2578
nobody-shea <82588993+nobody-shea@users.noreply.github.com>
2022-04-12 12:50:25 +0800

    Add files via upload

---
+++ encyclopedia/urls.py
@@ -0,0 +1,14 @@
+import imp
+from django.urls import re_path,path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    re_path(r"^wiki/(.+)/$", views.getEntry,name="entry"),
+    path("search", views.search, name="search"),
+    path("random", views.getRandomEntry, name="random"),
+    path("createPage", views.createPage, name="create"),
+    path("createEntry",views.createEntry,name="createEntry"),
+    re_path(r"^edit/(.+)/$", views.editPage, name="edit"),
+    path("editEntry",views.editEntry,name="editEntry"),
+]
---
+++ encyclopedia/util.py
@@ -0,0 +1,48 @@
+import re
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
+
+def list_entries():
+    """
+    Returns a list of all names of encyclopedia entries.
+    """
+    _, filenames = default_storage.listdir("entries")
+    return list(sorted(re.sub(r"\.md$", "", filename)
+                for filename in filenames if filename.endswith(".md")))
+
+def list_RelateEntries(name):
+    """
+    Returns a list of relate names of encyclopedia entries.
+    """
+    _, filenames = default_storage.listdir("entries")
+    ls  = list(sorted(re.sub(r"\.md$", "", filename)for filename in filenames if filename.endswith(".md")))
+
+    rl = [ filename for filename in ls if re.search(name,filename) is not None]
+
+    return rl
+    
+
+def save_entry(title, content):
+    """
+    Saves an encyclopedia entry, given its title and Markdown
+    content. If an existing entry with the same title already exists,
+    it is replaced.
+    """
+    filename = f"entries/{title}.md"
+    if default_storage.exists(filename):
+        default_storage.delete(filename)
+    default_storage.save(filename, ContentFile(content))
+
+
+def get_entry(title):
+    """
+    Retrieves an encyclopedia entry by its title. If no such
+    entry exists, the function returns None.
+    """
+    try:
+        f = default_storage.open(f"entries/{title}.md")
+        return f.read().decode("utf-8")
+    except FileNotFoundError:
+        return None
